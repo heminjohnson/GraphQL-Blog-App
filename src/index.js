600b347b560d45b56d8d9c43c4f0c5afe0c9f9a8
Hemin <hemin.johnson@diconium.com>
2019-08-08 17:43:18 +0200

    refactor: cleanup codes which are not required

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import myCurrentLocation, { getGreeting, message, name } from './myModule'
-import add, { subtract } from './math'
-
-console.log(message)
-console.log(name)
-console.log(myCurrentLocation)
-console.log(getGreeting('Jessica'))
-console.log(add(5,6))
-console.log(subtract(4,3))
