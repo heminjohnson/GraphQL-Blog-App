059524a75678597ba09dea3247aaa0844182b244
Hemin <hemin.johnson@diconium.com>
2019-08-11 00:00:06 +0200

    feat: add relationship between post and author

--- src/index.js
+++ src/index.js
@@ -22,17 +22,20 @@ const posts = [{
   id: '10',
   title: 'GraphQL 101',
   body: 'This is how to use GraphQL ...',
-  published: true
+  published: true,
+  author: '1'
 }, {
   id: '11',
   title: 'GraphQL 201',
   body: 'This is an advanced GraphQl post ...',
-  published: false
+  published: false,
+  author: '1'
 }, {
   id: '12',
   title: 'Programming Music',
   body: '',
-  published: false
+  published: false,
+  author: '2'
 }]
 
 // Type Definitions (schema)
@@ -56,6 +59,7 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
   }
 `
 
@@ -96,6 +100,13 @@ const resolvers = {
         title: '',
         published: false
       }
+    }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
     }
   }
 }
