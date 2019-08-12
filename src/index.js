fb72ec32715eac10f671b2710dc5e7207e451644
Hemin Johnson <achayan1992@gmail.com>
2019-08-12 15:38:30 +0200

    refactor: refactor the mutations with spread operator

--- src/index.js
+++ src/index.js
@@ -155,9 +155,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       }
 
       users.push(user)
@@ -173,10 +171,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args
       }
 
       posts.push(post)
@@ -193,9 +188,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args
       }
 
       comments.push(comment)
